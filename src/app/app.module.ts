f92672369ae43720581f03f149fe03bcf018f002
adiaz@mcclatchyservices.com <adiaz@mcclatchyservices.com>
2019-01-30 15:47:56 -0500

    Created the login UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { LoginComponent } from './login/login.component';
+
+var config = {
+  apiKey: "AIzaSyDY-hsuOdxUPn-WhIMX55-zJq-VOsRYClk",
+  authDomain: "angular-login-f0d7e.firebaseapp.com",
+  databaseURL: "https://angular-login-f0d7e.firebaseio.com",
+  projectId: "angular-login-f0d7e",
+  storageBucket: "angular-login-f0d7e.appspot.com",
+  messagingSenderId: "1086809272234"
+};
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(config),
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
